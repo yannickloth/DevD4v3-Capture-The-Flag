268c7461879d960192ef94a3acb3ebeaa6111ee5
Yannick Loth <727881+yannickloth@users.noreply.github.com>
2026-09-01 17:43:18 +0200

    Add direnv and flake dev shell with dotnet-sdk_11

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Capture-The-Flag development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      systems = [ "x86_64-linux" "aarch64-linux" ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in
+    {
+      devShells = forAllSystems (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              dotnet-sdk_11
+            ];
+          };
+        });
+    };
+}
