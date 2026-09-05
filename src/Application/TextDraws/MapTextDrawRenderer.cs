09f8640869e05fa5424fcc39cc3777e63aec2092
Yannick Loth <727881+yannickloth@users.noreply.github.com>
2026-09-05 09:23:19 +0200

    IVP: namespace split CTF.Application.Maps

--- src/Application/TextDraws/MapTextDrawRenderer.cs
+++ src/Application/TextDraws/MapTextDrawRenderer.cs
@@ -1,4 +1,4 @@
-﻿namespace CTF.Application.Maps;
+﻿namespace CTF.Application.TextDraws;
 
 /// <remarks>Change drivers: CD-34 (root; Textdraw API); CD-12 (map-rotation rules) → CD-34; CD-11 (map configuration) → CD-34</remarks>
 public class MapTextDrawRenderer
